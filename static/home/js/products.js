5324ea93db547a7b8a95b92906375dd12e3201bb
Apollo6333 <andreluiznunes1712@gmail.com>
2024-03-31 11:22:00 -0300

    as

--- static/home/js/products.js
+++ static/home/js/products.js
@@ -48,6 +48,7 @@ function validate(itens) {
 
     if (statusName == 1 && statusCategory == 1 && statusPrice == 1) {
         add(itens);
+        location.reload();
     } else {
         console.log("Deu merda");
     }
@@ -55,4 +56,89 @@ function validate(itens) {
 
 function add(itens) {
     localStorage.setItem(itens.nameProduct, JSON.stringify(itens));
+
 }
+
+function sortTable(option) {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.querySelector("table");
+    switching = true;
+
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("TD")[option].innerHTML.toLowerCase();
+            y = rows[i + 1].getElementsByTagName("TD")[option].innerHTML.toLowerCase();
+
+            if (option === 0) {
+                if (x > y) {
+                    shouldSwitch = true;
+                    break;
+                }
+            } else {
+                if (x < y) {
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
+}
+
+function retrieveAndDisplayItems() {
+    var table = document.querySelector("table");
+
+    for (var i = 0; i < localStorage.length; i++) {
+        var key = localStorage.key(i);
+        var item = JSON.parse(localStorage.getItem(key));
+
+        var row = table.insertRow(-1);
+
+        var cellName = row.insertCell(0);
+        var cellCategory = row.insertCell(1);
+        var cellPrice = row.insertCell(2);
+        var cellActions = row.insertCell(3);
+
+        cellName.innerHTML = item.nameProduct;
+        cellCategory.innerHTML = item.catProduct;
+        cellPrice.innerHTML = item.priceProduct;
+
+        var trashIcon = document.createElement("i");
+        trashIcon.className = "fa-solid fa-trash icons";
+        trashIcon.addEventListener("click", function() {
+            deleteItem(key);
+            location.reload();
+        });
+        cellActions.appendChild(trashIcon);
+
+        var editIcon = document.createElement("i");
+        editIcon.className = "fa-solid fa-pen-to-square icons";
+        editIcon.addEventListener("click", function() {
+            editItem(key);
+            location.reload();
+        });
+        cellActions.appendChild(editIcon);
+    }
+}
+
+document.getElementById("sortingOption").addEventListener("change", function() {
+    var sortOption = this.value;
+    var optionIndex;
+
+    if (sortOption === "A - Z" || sortOption === "Z - A") {
+        optionIndex = 0;
+    } else if (sortOption === "Menor Valor" || sortOption === "Maior Valor") {
+        optionIndex = 2;
+    }
+
+    sortTable(optionIndex);
+});
+
+window.onload = retrieveAndDisplayItems;
